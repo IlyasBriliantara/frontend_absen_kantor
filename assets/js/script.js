29008ea0dbb9681767e4ad93d7bf79aa89d9d36b
IlyasBriliantara <briliantarailyas@gmail.com>
2024-09-17 10:51:27 +0700

    6th commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-document.getElementById("toggleButton").addEventListener("click", function() {
+document.getElementById("toggleButton").addEventListener("click", function () {
     var sidebar = document.getElementById("sidebar");
     sidebar.classList.toggle("open");
 });
 
-document.getElementById("closeButton").addEventListener("click", function() {
+document.getElementById("closeButton").addEventListener("click", function () {
     var sidebar = document.getElementById("sidebar");
     sidebar.classList.remove("open");
 });
